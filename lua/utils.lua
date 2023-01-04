4173c03ba5ff716e7618ebb99353d68cb9c323a0
Yuto Nishida <yuto@berkeley.edu>
2023-01-03 23:37:22 -0800

    Workaround for os.execute for neovide

--- lua/utils.lua
+++ lua/utils.lua
@@ -3,12 +3,17 @@ local M = {}
 function M.is_system_in_dark_mode()
   if vim.fn.has('macunix') == 1 then
     -- mac os
-    local cmd = 'defaults read -g AppleInterfaceStyle'
-    local exit_code = os.execute(cmd) -- will succeed on Dark mode
-    return exit_code == 0
-  else
-    error("Not implemented for current OS", 2)
+    local cmd = [[zsh -c 'defaults read -g AppleInterfaceStyle']]
+    local handle = io.popen(cmd) -- os.execute crashes neovide; see https://github.com/neovide/neovide/issues/1376#issue-1289518816
+    if handle == nil then
+      error("Could not execute shell command", 2)
+    end
+    local res = handle:read("*a")
+    handle:close()
+    return res == 'Dark\n'
   end
+
+  error("Not implemented for current OS", 2)
 end
 
 return M
