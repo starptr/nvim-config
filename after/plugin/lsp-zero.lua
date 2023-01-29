8305bd565dcb97464ccb677486b965b7b4d72a4c
Yuto Nishida <yuto@berkeley.edu>
2023-01-28 17:51:52 -0800

    Organize nvim-cmp custom config into a module

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,3 +1,70 @@
+-- require('mason-nvim-dap').setup {
+--   automatic_setup = true,
+-- }
+
+-- local cpptools_debug_adapter_root = require('mason-registry').get_package('cpptools'):get_install_path()
+-- local cpptools_debug_adapter_dir = cpptools_debug_adapter_root .. '/extension/debugAdapters'
+-- local opendebugad7_exe = cpptools_debug_adapter_dir .. '/bin/OpenDebugAD7'
+-- local lldbmi_exe = cpptools_debug_adapter_dir .. '/lldb-mi/bin/lldb-mi'
+-- require('dap').adapters.cppdbg = {
+--   id = 'cppdbg',
+--   type = 'executable',
+--   command = lldbmi_exe,
+-- }
+
+-- local codelldb_debug_adapter_root = require('mason-registry').get_package('codelldb'):get_install_path()
+-- local codelldb_exe = codelldb_debug_adapter_root .. '/codelldb'
+-- require('dap').adapters.codelldb = {
+--   type = 'server',
+--   port = "${port}",
+--   executable = {
+--     -- CHANGE THIS to your path!
+--     command = codelldb_exe,
+--     args = {"--port", "${port}"},
+-- 
+--     -- On windows you may have to uncomment this:
+--     -- detached = false,
+--   }
+-- }
+-- 
+-- local dap = require('dap')
+-- dap.configurations.cpp = {
+--   --{
+--   --  name = "Specify executable",
+--   --  type = "cppdbg",
+--   --  request = "launch",
+--   --  program = function()
+--   --    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--   --  end,
+--   --  cwd = '${workspaceFolder}',
+--   --  stopAtEntry = true,
+--   --  MIMode = "lldb",
+--   --  miDebuggerPath = lldbmi_exe,
+--   --},
+--   {
+--     name = "Launch file",
+--     type = "codelldb",
+--     request = "launch",
+--     program = function()
+--       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/build/', 'file')
+--     end,
+--     cwd = '${workspaceFolder}/build',
+--     stopOnEntry = false,
+--   },
+--   -- {
+--   --   name = 'Attach to gdbserver :1234',
+--   --   type = 'cppdbg',
+--   --   request = 'launch',
+--   --   MIMode = 'gdb',
+--   --   miDebuggerServerAddress = 'localhost:1234',
+--   --   miDebuggerPath = '/usr/bin/gdb',
+--   --   cwd = '${workspaceFolder}',
+--   --   program = function()
+--   --     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--   --   end,
+--   -- },
+-- }
+
 local lsp = require('lsp-zero')
 lsp.set_preferences {
   suggest_lsp_servers = true,
@@ -39,87 +106,8 @@ local function check_back_space()
   end
 end
 
-local cmp_config = lsp.defaults.cmp_config({
-  sources = {
-    {name = 'copilot'},
-    --- These are the default sources for lsp-zero
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'buffer'},
-    {name = 'luasnip', keyword_length = 2},
-  },
-  view = {
-    entries = {name = 'custom', selection_order = 'near_cursor' }
-  },
-  mapping = {
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-    -- ['<C-y>'] = cmp.mapping.confirm({select = false}),
-
-    -- navigate items on the list
-    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
-    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
-    -- ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
-    -- ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
-
-    -- scroll up and down in the completion documentation
-    ['<C-f>'] = cmp.mapping.scroll_docs(3)  ,
-    ['<C-b>'] = cmp.mapping.scroll_docs(-3) ,
-    -- ['<C-d>'] = cmp.mapping.scroll_docs(5)  ,
-    -- ['<C-u>'] = cmp.mapping.scroll_docs(-5) ,
-
-    -- toggle completion
-    --
-    ['<C-s>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.close()
-      else
-        cmp.complete()
-      end
-    end),
-
-    -- go to next placeholder in the snippet
-    ['<C-l>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-
-    -- go to previous placeholder in the snippet
-    ['<C-h>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-
-    -- when menu is visible, navigate to next item
-    -- when line is empty, insert a tab character
-    -- else, activate completion
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif check_back_space() then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, {'i', 's'}),
-
-    -- when menu is visible, navigate to previous item on list
-    -- else, revert to default behavior
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item(select_opts)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-  },
-})
-cmp.setup(cmp_config)
+local ncb = require('nvim-cmp-better')
+ncb.setup(ncb.get_default_custom_cmp_config_ext())
 
 -- insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
