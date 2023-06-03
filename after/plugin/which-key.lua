83a74c542a9d9523f5a3adc71e14642bbe85fed7
Yuto Nishida <yuto@berkeley.edu>
2023-06-02 19:26:55 -0700

    Use git_files when possible

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -40,7 +40,16 @@ local which_key_tables = {
   f = {
     name = "+search",
     g = { require('telescope.builtin').live_grep, 'Live grep' },
-    f = { '<cmd>Telescope find_files<cr>', 'Search files'},
+    -- f = { '<cmd>Telescope find_files<cr>', 'Search files'},
+    f = { function()
+      -- If inside a git repo, use git_files instead of find_files
+      vim.fn.system('git rev-parse --is-inside-work-tree')
+      if vim.v.shell_error == 0 then
+        require"telescope.builtin".git_files({})
+      else
+        require"telescope.builtin".find_files({})
+      end
+    end, 'Search files'},
     a = { function() require('telescope.builtin').find_files({
       find_command = { 'rg', '--files', '--color', 'never', '--hidden', '--glob', '!**/.git/*' },
     }) end , 'Search all files' },
